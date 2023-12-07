baf73e89c127647696c17700a1d4cb0b7b1342e9
Artem Shevchuk <>
2023-12-07 23:54:14 +0200

    change directory with netlify functions config

--- functions/express.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const serverless = require('serverless-http');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Express on Netlify with AWS Lambda!' });
-});
-
-module.exports.handler = serverless(app);
