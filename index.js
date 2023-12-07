a5628b9060f0685c9b81c949185c43b3719b667a
Artem Shevchuk <>
2023-12-07 20:23:02 +0200

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const config = require("config");
+const cors = require("cors");
+
+const User = require("./modules/user/routes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({ extended: true }));
+app.use("/api/v1/users", User);
+
+const PORT = process.env.PORT || config.get("port");
+const HOST = "localhost";
+
+async function start() {
+  try {
+    app.listen(PORT, () =>
+      console.log(`Server running at http://${HOST}:${PORT}/`)
+    );
+  } catch (e) {
+    console.log("Server Error", e.message);
+    process.exit(1);
+  }
+
+  app.get("/", function (req, res) {
+    res.json("Cors enabled");
+  });
+}
+
+start();
